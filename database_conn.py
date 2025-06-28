fa9a947f253177e56f866b4eaa5ad71cf530c15d
24mca030@nirmauni.ac.in <24mca030@nirmauni.ac.in>
2025-06-28 19:13:14 +0530

    🔧 Add DB env support and render deployment config

--- database_conn.py
+++ database_conn.py
@@ -1,12 +1,26 @@
-import mysql
+import os
 import mysql.connector
-try:
-    def get_connection():
-        return mysql.connector.connect(
-            user = "root",
-        host = "localhost",
-        password = "",
-        database = "automobile_shop"
-        )
-except:
-    print("No connection")
+
+def get_connection():
+    return mysql.connector.connect(
+        host=os.environ.get("DB_HOST", "localhost"),
+        user=os.environ.get("DB_USER", "root"),
+        password=os.environ.get("DB_PASSWORD", ""),
+        database=os.environ.get("DB_NAME", "automobile_shop")
+    )
+
+
+
+
+# import mysql
+# import mysql.connector
+# try:
+#     def get_connection():
+#         return mysql.connector.connect(
+#             user = "root",
+#         host = "localhost",
+#         password = "",
+#         database = "automobile_shop"
+#         )
+# except:
+#     print("No connection")
